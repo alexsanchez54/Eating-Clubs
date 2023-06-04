71a0dbb5c61d740e6b60f280d021b0aa0d65dbc6
alexsanchez54 <76659665+alexsanchez54@users.noreply.github.com>
2023-06-03 20:59:12 -0400

    Add files via upload

--- eating_clubs_analysis_new.R
+++ eating_clubs_analysis_new.R
@@ -14,6 +14,7 @@ library(htmltools)
 library(lsr)
 library(effsize)
 library(plotly)
+library(lemon)
 
 # To-do:
 # Do Mummolo-style placebo checks
@@ -163,140 +164,13 @@ alldata_summary |>
   group_by(treated_days) |> 
   summarize(avg = mean(not_null))
 
-summary(lm(not_null ~ treated_days,
-           data = alldata_summary))
-
-summary(lm(not_null ~ deans_date + pledge + treated_days + post_deansdate,
-           data = alldata_summary))
-
-summary(lm(not_null ~ deans_date + pledge + treated_days + spring_break + post_deansdate,
-           data = alldata_summary))
+######## Initial regressions #####
 
 #May
-summary(lm(not_null ~ deans_date + pledge + treated_days  + post_deansdate,
+summary(lm(not_null ~ deans_date + pledge + treated_days + post_deansdate,
            data = alldata_summary |> filter(month == 5)))
 
-#mid-April through end of May
-summary(lm(not_null ~ deans_date + pledge + treated_days + post_deansdate, 
-           data = alldata_summary |> filter(month(infr_date) %in% c(4, 5) & 
-                                              (month(infr_date) != 4 | day(infr_date) >= 15))))
-
-#April and May
-summary(lm(not_null ~ deans_date + pledge + treated_days + post_deansdate,
-           data = alldata_summary |> filter(month(infr_date) %in% c(4, 5))))
-
-#mid-March through end of May
-summary(lm(not_null ~ deans_date + pledge + treated_days + spring_break + post_deansdate, 
-           data = alldata_summary |> filter(month(infr_date) %in% c(3, 4, 5) & 
-                                              (month(infr_date) != 3 | day(infr_date) >= 11))))
-
-#March - May (includes Spring Break control)
 summary(lm(not_null ~ deans_date + pledge + treated_days + spring_break + post_deansdate,
-           data = alldata_summary |> filter(month(infr_date) %in% c(3, 4, 5))))
-
-#Feb - May (includes Spring Break control)
-summary(lm(not_null ~ deans_date + pledge + treated_days + spring_break + post_deansdate,
-           data = alldata_summary |> filter(month(infr_date) %in% c(2, 3, 4, 5))))
-
-#Jan - May (all of Spring semester; includes Spring Break control)
-summary(lm(not_null ~  deans_date + pledge + treated_days + spring_break + post_deansdate, 
-           data = alldata_summary))
-
-### Compute effect size
-cohen.d(alldata_summary |> filter(treated_days == 0 & month(infr_date) %in% 5) |> pull(not_null),
-  alldata_summary |> filter(treated_days == 1 & month(infr_date) %in% 5) |> pull(not_null))
-
-cohen.d(alldata_summary |> filter(treated_days == 0) |> pull(not_null),
-        alldata_summary |> filter(treated_days == 1) |> pull(not_null))
-
-cohensD(not_null ~ treated_days, data = alldata_summary)
-
-cohensD(not_null ~ treated_days, data = droplevels(subset(alldata_summary, month(infr_date) %in% 5)))
-
-
-# standard deviation
-sd(alldata_summary$not_null)
-
-
-####### Placebo Checks #######
-
-#remove leap days for now for simplicity (Alex fix this later)
-alldata_summary$leap_days <- ifelse(month(alldata_summary$infr_date) == 2 
-                                    & day(alldata_summary$infr_date) == 29, 
-                                    1, 0)
-
-alldata_summary <- alldata_summary |> 
-  filter(leap_days != 1)
-
-#create a variable of sequence of days by year
-alldata_summary <- alldata_summary |>
-  group_by(year) |> 
-  mutate(day_by_year = 1:n())
-
-#change # of treated days (From 17 to 12, 7, or 2)
-alldata_summary <- alldata_summary |> 
-  mutate(treated_days = if_else(year == 2018 & day_by_year >= (135 - 0) & day_by_year <= (151 - 0), 1, 0))
-
-summary(lm(not_null ~ deans_date + pledge + treated_days + post_deansdate, 
-           data = alldata_summary |> filter(day_by_year >= (1))))
-
-summary(lm(null ~ deans_date + pledge + treated_days + post_deansdate, 
-           data = alldata_summary |> filter(day_by_year >= (1))))
-
-# Date range
-dates <- seq.Date(as.Date("2018-05-14"), length.out = 135, by = "-1 day")
-
-# Initialize an empty dataframe to store results
-results_df <- data.frame()
-
-# Loop over 1 to 135
-for (i in 1:135) {
-  
-  # Fit the model
-  fit <- lm(not_null ~ deans_date + pledge + treated_days + post_deansdate, 
-            data = alldata_summary |> filter(day_by_year >= (135 - i)))
-  
-  # Get the coefficients
-  coef_summary <- summary(fit)$coefficients
-  
-  # Get the p-value for treated_days
-  p_value <- coef_summary["treated_days", "Pr(>|t|)"]
-  
-  # Transpose the result and convert it to a dataframe
-  df <- data.frame(p_value)
-  
-  # Add a column to indicate the iteration
-  df$pre_pledge_cutoff <- dates[i]
-  
-  # Append the results to results_df
-  results_df <- rbind(results_df, df)
-}
-
-#remove extra date
-results_df <- results_df |> 
-  filter(year(pre_pledge_cutoff) == 2018)
-
-####
-
-plot <- 
-  results_df |> 
-  ggplot(aes(x = pre_pledge_cutoff, y = p_value)) +
-  theme(axis.text.x = element_text(angle = 90, size = 5)) +
-  theme(axis.text.y = element_text(size = 5)) +
-  geom_point(size = 1) +
-  geom_hline(yintercept = 0.05, linetype = 2, color = "red") +
-  scale_x_date(date_labels = "%d %b", date_breaks = "1 day", expand = c(0,0)) +
-  scale_y_continuous(limits = c(0.025, 0.125), breaks = seq(0.005, 0.125, by = 0.005)) +
-  labs(title = "test",
-       x = "Pre-pledge cutoff date",
-       y = "P value")
-
-plotly <- ggplotly(plot)
-
-########
-
-#May
-summary(lm(not_null ~ deans_date + pledge + treated_days + post_deansdate,
            data = alldata_summary |> filter(month == 5)))
 
 #mid-April through end of May
@@ -336,3 +210,264 @@ summary(lm(not_null ~  deans_date + pledge + treated_days + spring_break + post_
 summary(lm(not_null ~  deans_date + pledge + treated_days + spring_break + post_deansdate, 
            data = alldata_summary))
 
+summary(lm(null ~  deans_date + pledge + treated_days + spring_break + post_deansdate, 
+           data = alldata_summary))
+
+
+### Compute effect size
+
+cohensD(not_null ~ treated_days, data = alldata_summary)
+
+cohensD(not_null ~ treated_days, data = droplevels(subset(alldata_summary, month(infr_date) %in% 5)))
+
+# standard deviation
+sd(alldata_summary$not_null)
+
+####### Placebo Checks #######
+
+#remove leap days for now for simplicity (Alex fix this later)
+alldata_summary$leap_days <- ifelse(month(alldata_summary$infr_date) == 2 
+                                    & day(alldata_summary$infr_date) == 29, 
+                                    1, 0)
+
+alldata_summary <- alldata_summary |> 
+  filter(leap_days != 1)
+
+#create a variable of sequence of days by year
+alldata_summary <- alldata_summary |>
+  group_by(year) |> 
+  mutate(day_by_year = 1:n())
+
+summary(lm(not_null ~ deans_date + pledge + treated_days + post_deansdate + spring_break, 
+           data = alldata_summary |> filter(day_by_year >= (1))))
+
+###### Plot p values for not_null #####
+
+# Initialize an empty dataframe to store results
+results_df <- data.frame()
+
+# Date range
+dates <- seq.Date(as.Date("2018-05-14"), length.out = 135, by = "-1 day")
+
+# Loop over 1 to 135
+for (i in 1:135) {
+  
+  # Filter the data
+  filtered_data <- alldata_summary |> filter(day_by_year >= (135 - i))
+  
+  # include spring_break as a control when appropriate
+  formula <- if (length(unique(filtered_data$spring_break)) == 2) {
+    not_null ~ deans_date + pledge + treated_days + post_deansdate + spring_break
+  } else {
+    not_null ~ deans_date + pledge + treated_days + post_deansdate
+  }
+  
+  # Fit the model
+  fit <- lm(formula, data = filtered_data)
+  
+  # Get the coefficients
+  coef_summary <- summary(fit)$coefficients
+  
+  # Get the p-value for treated_days
+  p_value <- coef_summary["treated_days", "Pr(>|t|)"]
+  
+  # Calculate Cohen's D
+  cohens_d <- cohensD(not_null ~ treated_days, data = filtered_data)
+  
+  # Transpose the result and convert it to a dataframe
+  df <- data.frame(p_value, cohens_d)
+  
+  # Add a column to indicate the iteration
+  df$pre_pledge_cutoff <- dates[i]
+  
+  # Append the results to results_df
+  results_df <- rbind(results_df, df)
+}
+
+#remove extra dates
+results_df <- results_df |> 
+  filter(year(pre_pledge_cutoff) == 2018)
+
+###
+
+plot <- 
+  results_df |> 
+  ggplot(aes(x = pre_pledge_cutoff, y = p_value)) +
+  theme(axis.text.x = element_text(angle = 90, size = 5)) +
+  theme(axis.text.y = element_text(size = 5)) +
+  geom_point(aes(alpha = cohens_d), size = 2) +
+  geom_hline(yintercept = 0.05, linetype = 2, color = "red") +
+  #facet_rep_wrap(~ post_pledge_cutoff, repeat.tick.labels = TRUE, ncol = 1, scales = "free_x") +
+  scale_x_date(date_labels = "%d %b", date_breaks = "1 day", expand = c(0,0)) +
+  scale_y_continuous(limits = c(0.025, 0.125), breaks = seq(0.005, 0.125, by = 0.005)) +
+  labs(title = "P values for the effect of the pledge on subsequent # of infractions through the rest of Spring semester 2018, by pre-pledge cutoff date",       x = "Pre-pledge cutoff date",
+       y = "P value",
+       alpha = "Cohen's D")
+
+plotly <- ggplotly(plot)
+
+####Include p values for null ####
+
+# Initialize an empty dataframe to store results
+results_df <- data.frame()
+
+# Date range
+dates <- seq.Date(as.Date("2018-05-14"), length.out = 135, by = "-1 day")
+
+# Loop over 1 to 135
+for (i in 1:135) {
+  
+  # Filter the data
+  filtered_data <- alldata_summary |> filter(day_by_year >= (135 - i))
+  
+  # Determine the formula to use based on the number of levels of spring_break in the filtered data
+  formula_not_null <- if (length(unique(filtered_data$spring_break)) == 2) {
+    not_null ~ deans_date + pledge + treated_days + post_deansdate + spring_break
+  } else {
+    not_null ~ deans_date + pledge + treated_days + post_deansdate
+  }
+  
+  formula_null <- if (length(unique(filtered_data$spring_break)) == 2) {
+    null ~ deans_date + pledge + treated_days + post_deansdate + spring_break
+  } else {
+    null ~ deans_date + pledge + treated_days + post_deansdate
+  }
+  
+  # Fit the models
+  fit_not_null <- lm(formula_not_null, data = filtered_data)
+  fit_null <- lm(formula_null, data = filtered_data)
+  
+  # Get the coefficients
+  coef_summary_not_null <- summary(fit_not_null)$coefficients
+  coef_summary_null <- summary(fit_null)$coefficients
+  
+  # Get the p-value for treated_days
+  p_value_not_null <- coef_summary_not_null["treated_days", "Pr(>|t|)"]
+  p_value_null <- coef_summary_null["treated_days", "Pr(>|t|)"]
+  
+  # Calculate Cohen's D
+  cohens_d_not_null <- cohensD(not_null ~ treated_days, data = filtered_data)
+  cohens_d_null <- cohensD(null ~ treated_days, data = filtered_data)
+  
+  # Transpose the result and convert it to a dataframe
+  df <- data.frame(p_value, cohens_d)
+  
+  # Create a dataframe for the results
+  df_not_null <- data.frame(p_value = p_value_not_null, cohens_d = cohens_d_not_null, outcome = "not_null", pre_pledge_cutoff = dates[i])
+  df_null <- data.frame(p_value = p_value_null, cohens_d = cohens_d_null, outcome = "null", pre_pledge_cutoff = dates[i])
+  
+  # Append the results to results_df
+  results_df <- rbind(results_df, df_not_null, df_null)
+}
+
+plot <- 
+  results_df |> 
+  ggplot(aes(x = pre_pledge_cutoff, y = p_value, color = outcome)) +
+  theme(axis.text.x = element_text(angle = 90, size = 5)) +
+  theme(axis.text.y = element_text(size = 5)) +
+  geom_point(aes(alpha = cohens_d), size = 2) +
+  geom_hline(yintercept = 0.05, linetype = 2, color = "red") +
+  scale_x_date(date_labels = "%d %b", date_breaks = "1 day", expand = c(0,0)) +
+  labs(title = "P values for the effect of the pledge on subsequent # of infractions through the rest of Spring semester 2018, by pre-pledge cutoff date",
+       caption = "different colors indicate type of infraction (those we expect to be influenced by the treatment vs not)",
+       x = "Pre-pledge cutoff date",
+       y = "P value")
+
+plotly1 <- ggplotly(plot)
+
+###
+
+##### Plot p values for not_null at different bandwidths ####
+
+# Initialize an empty dataframe to store results
+results_df <- data.frame()
+
+# Date range
+dates_pre <- seq.Date(as.Date("2018-05-14"), length.out = 135, by = "-1 day")
+dates_post <- c("May 31 2018: 16 days after pledge (rest of Spring semester)", 
+                "2018-05-26: 11 days after pledge", 
+                "2018-05-21: 6 days after pledge")
+
+# Outer loop
+for (j in 1:3) {
+  # Alter treated_days based on j
+  alldata_summary <- alldata_summary |> 
+    mutate(treated_days = if_else(year == 2018 & day_by_year >= 135 & day_by_year <= (156 - 5*j), 1, 0))
+  
+  # Inner loop
+  for (i in 1:135) {
+    
+    # Filter the data
+    filtered_data <- alldata_summary |> filter(day_by_year >= (135 - i))
+    
+    # include spring_break as a control when appropriate
+    formula <- if (length(unique(filtered_data$spring_break)) == 2) {
+      not_null ~ deans_date + pledge + treated_days + post_deansdate + spring_break
+    } else {
+      not_null ~ deans_date + pledge + treated_days + post_deansdate
+    }
+    
+    # Fit the model
+    fit <- lm(formula, data = filtered_data)
+    
+    # Get the coefficients
+    coef_summary <- summary(fit)$coefficients
+    
+    # Get the p-value for treated_days
+    p_value <- coef_summary["treated_days", "Pr(>|t|)"]
+    
+    # Calculate Cohen's D
+    cohens_d <- cohensD(not_null ~ treated_days, data = filtered_data)
+    
+    # Transpose the result and convert it to a dataframe
+    df <- data.frame(p_value, cohens_d)
+    
+    # Add a column to indicate the iteration of i
+    df$pre_pledge_cutoff <- dates_pre[i]
+    
+    # Add a column to indicate the iteration of j
+    df$post_pledge_cutoff <- dates_post[j]
+    
+    # Append the results to results_df
+    results_df <- rbind(results_df, df)
+  }
+}
+
+#remove extra dates
+results_df <- results_df |> 
+  filter(year(pre_pledge_cutoff) == 2018)
+
+results_df <- results_df |> 
+  mutate(post_pledge_cutoff = factor(post_pledge_cutoff, 
+                                     levels = c(
+                                       "May 31 2018: 16 days after pledge (rest of Spring semester)", 
+                                       "2018-05-26: 11 days after pledge", 
+                                       "2018-05-21: 6 days after pledge")))
+
+plot <- 
+  results_df |> 
+  ggplot(aes(x = pre_pledge_cutoff, y = p_value)) +
+  theme(axis.text.x = element_text(angle = 90, size = 5)) +
+  theme(axis.text.y = element_text(size = 5)) +
+  geom_point(aes(alpha = cohens_d), size = 2) +
+  geom_hline(yintercept = 0.05, linetype = 2, color = "red") +
+  facet_rep_wrap(~ post_pledge_cutoff, repeat.tick.labels = TRUE, ncol = 1, scales = "free_x") +
+  scale_x_date(date_labels = "%d %b", date_breaks = "1 day", expand = c(0,0)) +
+  #scale_y_continuous(limits = c(0.025, 0.125), breaks = seq(0.005, 0.125, by = 0.005)) +
+  labs(title = "P values for the effect of the pledge on subsequent # of infractions at different post-pledge cutoff dates, by pre-pledge cutoff date",
+       caption = "The more opaque the points, the larger the Cohen's d effect size",
+       x = "Pre-pledge cutoff date",
+       y = "P value",
+       alpha = "Cohen's D")
+
+plotly2 <- ggplotly(plot)
+
+
+######
+
+
+summary(lm(not_null ~ deans_date + pledge + treated_days + post_deansdate + spring_break,
+           data = alldata_summary))
+
+summary(lm(not_null ~ deans_date + pledge + treated_days + post_deansdate + spring_break,
+           data = alldata_summary |> filter(month %in% c(3, 4, 5))))
